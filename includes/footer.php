8ff28dbfe26cc0cf4dc8050320ab722f77129abf
Emeti <emetisuccess@gmail.com>
2022-03-20 20:13:41 +0100

    commited

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<footer class="text-center text-white bg-info pt-3">
+    <!-- Copyright -->
+    <div class="text-center p-4">
+        <span>© 2020 Copyright: Emedev</span>
+    </div>
+    <!-- Copyright -->
+</footer>
+<!-- Footer -->
+</body>
+
+</html>
