314b19795cbf01c512fd2cf82f00114f745704a9
Emeti <emetisuccess@gmail.com>
2022-03-22 22:56:29 +0100

    committed

---
+++ includes/contact.php
@@ -0,0 +1,41 @@
+<?php
+if (isset($_POST["send"])) {
+
+    $to = "emetisuccess@gmail.com";
+    $subject = wordwrap($_POST["subject"], 70);
+    $body = $_POST["body"];
+    $header = $_POST["email"];
+
+    mail($to, $subject, $body, $header);
+
+    header("location: index.php");
+}
+
+?>
+
+<div class="header mt-4">
+    <h4 class="text-center">Contact me</h4>
+</div>
+<div class="row justify-content-center mt-2">
+    <div class="col-md-8">
+        <div class="card border-0">
+            <div class="card-body">
+                <form action="" method="post">
+                    <div class="form-group">
+                        <input type="text" name="subject" class="form-control" placeholder="Enter Subject">
+                    </div>
+                    <div class="form-group">
+                        <input type="email" name="email" class="form-control" placeholder="Email">
+                    </div>
+                    <div class="form-group">
+                        <textarea name="message" placeholder="Message" id="" cols="30" rows="4"
+                            class="form-control"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" name="send" value="Send" class="btn btn-info btn-block">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -171,32 +171,7 @@
         </div>
     </div>
     <div class="contact" id="contact">
-        <div class="header mt-4">
-            <h4 class="text-center">Contact me</h4>
-        </div>
-        <div class="row justify-content-center mt-2">
-            <div class="col-md-8">
-                <div class="card border-0">
-                    <div class="card-body">
-                        <form action="message.php" method="post">
-                            <div class="form-group">
-                                <input type="text" name="name" class="form-control" placeholder="Name">
-                            </div>
-                            <div class="form-group">
-                                <input type="email" name="email" class="form-control" placeholder="Email">
-                            </div>
-                            <div class="form-group">
-                                <textarea name="message" placeholder="Message" id="" cols="30" rows="4"
-                                    class="form-control"></textarea>
-                            </div>
-                            <div class="form-group">
-                                <input type="submit" name="send" value="Send" class="btn btn-info btn-block">
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <?php include("includes/contact.php"); ?>
     </div>
 </div>
 <?php include("includes/footer.php"); ?>
