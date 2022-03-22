2c7400720a330585fd97386d8e783add8b041dfb
Emeti <emetisuccess@gmail.com>
2022-03-22 08:25:05 +0100

    committd

--- includes/header.php
+++ includes/header.php
@@ -1,33 +1,16 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html lang="en">
+ <!DOCTYPE html>
+ <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="fontawesome/css/font-awesome.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-        <link rel="stylesheet" href="style/bootstrap.css">
-        <link rel="stylesheet" href="style/style.css">
-        <title>Portfolio</title>
-    </head>
+     <head>
+         <meta charset="UTF-8">
+         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         <link rel="stylesheet" href="fontawesome/css/font-awesome.css">
+         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
+         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+         <link rel="stylesheet" href="style/bootstrap.css">
+         <link rel="stylesheet" href="style/style.css">
+         <title>Portfolio</title>
+     </head>
 
-=======
-<!DOCTYPE html>
-<html lang="en">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="fontawesome/css/font-awesome.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
-        <link rel="stylesheet" href="style/bootstrap.css">
-        <link rel="stylesheet" href="style/style.css">
-        <title>Portfolio</title>
-    </head>
-
->>>>>>> 6b8b2b9d32abc3f559081f7bf79cd11a67434a04
-    <body>
+     <body>
