507fbf2fd5a94cd22d0f52a55fa84f8c1a690246
Emeti <emetisuccess@gmail.com>
2022-03-22 19:45:34 +0100

    index added

--- includes/footer.php
+++ includes/footer.php
@@ -6,6 +6,24 @@
     <!-- Copyright -->
 </footer>
 <!-- Footer -->
+<script src="../js/jquery-3.6.0.min.js"></script>
+<script>
+$(document).ready(function() {
+    $('a').on('click', function(event) {
+
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 50000, function() {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
+</script>
 </body>
 
 </html>
