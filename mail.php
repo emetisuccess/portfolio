92e9057ea2df2ee5a018311765a8b4a6183af373
Emeti <emetisuccess@gmail.com>
2022-04-27 01:41:02 +0100

    changes made

--- mail.php
+++ mail.php
@@ -1,5 +1,17 @@
 <?php
 
+// the message
+// $msg = "First line of text\nSecond line of text";
+
+// // use wordwrap() if lines are longer than 70 characters
+// $msg = wordwrap($msg, 70);
+
+// // send email
+// mail("emetisuccess@gmail.com", "Emeti Etim is trying very hard", $msg);
+
+
+
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
@@ -8,50 +20,34 @@ require_once __DIR__ . '/vendor/phpmailer/src/Exception.php';
 require_once __DIR__ . '/vendor/phpmailer/src/PHPMailer.php';
 require_once __DIR__ . '/vendor/phpmailer/src/SMTP.php';
 
+// passing true in constructor enables exceptions in PHPMailer
+$mail = new PHPMailer(true);
 
-if (isset($_POST["send"])) {
+try {
+    // Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER; // for detailed debug output
+    $mail->isSMTP();
+    $mail->Host = 'smtp.mailtrap.io';
+    $mail->Username = 'a6501e4ca437da'; // YOUR gmail email
+    $mail->Password = '2e8fa7e22d2644'; // YOUR gmail password
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 2525;
 
-    // $to = "emetisuccess@gmail.com";
-    // $subject = wordwrap($_POST["subject"], 70);
-    // $body = $_POST["message"];
-    // $header = $_POST["email"];
-    // $mail = mail($to, $subject, $body, $header);
 
-    // if ($mail == true) {
-    //     echo "Message sent successfully...";
-    // } else {
-    //     echo "Message could not be sent: {$mail->ErrorInfo}";
-    // }
+    // Sender and recipient settings
+    $mail->setFrom('emecoder@gmail.com', 'Emetisuccess');
+    $mail->addAddress('emetisuccess@gmail.com', 'Emeti Etim');
+    $mail->addReplyTo('emecoder@gmail.com', 'Emetisuccess'); // to set the reply to
 
-    // passing true in constructor enables exceptions in PHPMailer
+    // Setting the email content
+    $mail->IsHTML(true);
+    $mail->Subject = "Testing Email sending format";
+    $mail->Body = 'i love HTML message body. <b>Gmail</b> SMTP email body.';
+    $mail->AltBody = 'Plain text message body for non-HTML email client. Gmail SMTP email body.';
 
-    $mail = new PHPMailer(true);
-
-    try {
-        // Server settings
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER; // for detailed debug output
-        $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';
-        $mail->Username = 'emetisuccess@gmail.com'; // YOUR gmail email
-        $mail->Password = 'jcwtxblevtdicyqo'; // YOUR gmail password
-        $mail->SMTPAuth = true;
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $mail->Port = 587;
-
-        // Sender and recipient settings
-        $mail->setFrom('emecoder@gmail.com', 'Emetisuccess');
-        $mail->addAddress('emetisuccess@gmail.com', 'Emeti Etim');
-        $mail->addReplyTo('emecoder@gmail.com', 'Emetisuccess'); // to set the reply to
-
-        // Setting the email content
-        $mail->IsHTML(true);
-        $mail->Subject = "Testing Email sending format";
-        $mail->Body = 'i love HTML message body. <b>Gmail</b> SMTP email body.';
-        $mail->AltBody = 'Plain text message body for non-HTML email client. Gmail SMTP email body.';
-
-        $mail->send();
-        echo "Email message sent.";
-    } catch (Exception $e) {
-        echo "Error in sending email. Mailer Error: {$mail->ErrorInfo}";
-    }
+    $mail->send();
+    echo "Email message sent.";
+} catch (Exception $e) {
+    echo "Error in sending email. Mailer Error: {$mail->ErrorInfo}";
 }
